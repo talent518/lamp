63c1b4b04748206579ec5b5baaffd1fa3652407f
阿宝 <talent518@yeah.net>
2021-03-17 18:13:10 +0800

    php-8.0.x的编译。

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ linux apache mysql php server
 
 ### 注意
 * php.ini配置中opcache.protect_memory=0，如果设置为1会导航段错误，原因：这个参数是非线程安全的开关
+* php-8.0.x的编译：LAMP=lamp8 PHP_VER=8.0.3 ./lamp-setup.sh
 
--- lamp-setup.sh
+++ lamp-setup.sh
@@ -2,22 +2,28 @@
 
 set -e
 
+if test -z "$LAMP"; then
+	LAMP=lamp
+fi
+
 APR_VER=1.7.0
 APR_ICONV_VER=1.2.2
 APR_UTIL_VER=1.6.1
 HTTPD_VER=2.4.46
+if test -z "$PHP_VER"; then
 PHP_VER=7.4.11
+fi
 MY_VER=10.5.6
 
 #############################################
 # apr
 #############################################
 
-if test ! -f /opt/lamp/lib/libapr-1.so; then
+if test ! -f /opt/$LAMP/lib/libapr-1.so; then
 	test ! -d /tmp/apr-$APR_VER && tar -xvf apr-$APR_VER.tar.bz2 -C /tmp
 
 	pushd /tmp/apr-$APR_VER
-	test ! -f Makefile && ./configure --prefix=/opt/lamp --enable-threads --enable-posix-shm --with-sendfile
+	test ! -f Makefile && ./configure --prefix=/opt/$LAMP --enable-threads --enable-posix-shm --with-sendfile
 	make -j4
 	make install
 	popd
@@ -27,11 +33,11 @@ fi
 # apr-iconv
 #############################################
 
-if test ! -f /opt/lamp/lib/libapriconv-1.so; then
+if test ! -f /opt/$LAMP/lib/libapriconv-1.so; then
 	test ! -d /tmp/apr-iconv-$APR_ICONV_VER && tar -xvf apr-iconv-$APR_ICONV_VER.tar.bz2 -C /tmp
 
 	pushd /tmp/apr-iconv-$APR_ICONV_VER
-	test ! -f Makefile && ./configure --prefix=/opt/lamp --with-apr=/opt/lamp
+	test ! -f Makefile && ./configure --prefix=/opt/$LAMP --with-apr=/opt/$LAMP
 	make -j4
 	make install
 	popd
@@ -41,11 +47,11 @@ fi
 # apr-util
 #############################################
 
-if test ! -f /opt/lamp/lib/libaprutil-1.so; then
+if test ! -f /opt/$LAMP/lib/libaprutil-1.so; then
 	test ! -d /tmp/apr-util-$APR_UTIL_VER && tar -xvf apr-util-$APR_UTIL_VER.tar.bz2 -C /tmp
 
 	pushd /tmp/apr-util-$APR_UTIL_VER
-	test ! -f Makefile && ./configure --prefix=/opt/lamp --with-apr=/opt/lamp
+	test ! -f Makefile && ./configure --prefix=/opt/$LAMP --with-apr=/opt/$LAMP
 	make -j4
 	make install
 	popd
@@ -55,11 +61,11 @@ fi
 # httpd
 #############################################
 
-if test ! -f /opt/lamp/bin/httpd; then
+if test ! -f /opt/$LAMP/bin/httpd; then
 	test ! -d /tmp/httpd-$HTTPD_VER && tar -xvf httpd-$HTTPD_VER.tar.bz2 -C /tmp
 
 	pushd /tmp/httpd-$HTTPD_VER
-	test ! -f Makefile && ./configure --prefix=/opt/lamp --with-apr=/opt/lamp --with-apr-util=/opt/lamp --enable-load-all-modules --enable-mpms-shared=all
+	test ! -f Makefile && ./configure --prefix=/opt/$LAMP --with-apr=/opt/$LAMP --with-apr-util=/opt/$LAMP --enable-load-all-modules --enable-mpms-shared=all
 	make -j4
 	make install
 	popd
@@ -69,16 +75,28 @@ fi
 # php
 #############################################
 
-if test ! -f /opt/lamp/modules/libphp7.so; then
+if test "${PHP_VER:0:3}" = "8.0"; then
+	PHP_SO=php
+else
+	PHP_SO=php7
+fi
+
+if test ! -f /opt/$LAMP/modules/lib$PHP_SO.so; then
 	test ! -d /tmp/php-$PHP_VER && tar -xvf php-$PHP_VER.tar.bz2 -C /tmp
 
+	if test "${PHP_VER:0:3}" = "8.0"; then
+		PHP_CFG="--enable-zts --enable-phpdbg --with-openssl --with-kerberos --with-system-ciphers --with-zlib --enable-bcmath --with-bz2 --enable-calendar --with-curl --enable-dba=shared --with-enchant --enable-exif --with-ffi --enable-ftp --enable-gd --with-external-gd --with-webp --with-jpeg --with-xpm --with-freetype --enable-gd-jis-conv --with-gettext --with-gmp --with-mhash --with-imap --with-kerberos --with-imap-ssl --enable-intl --with-ldap --with-ldap-sasl --enable-mbstring --with-mysqli --enable-pcntl --with-pdo-mysql --with-pspell --with-libedit --with-readline --enable-shmop --with-snmp --enable-soap --enable-sockets --enable-sysvmsg --enable-sysvsem --enable-sysvshm --with-tidy --with-expat --with-xsl --enable-zend-test=shared --with-zip --enable-mysqlnd"
+	else
+		PHP_CFG="--enable-inline-optimization --enable-maintainer-zts --with-tsrm-pthreads --enable-phpdbg --with-openssl --with-kerberos --with-system-ciphers --with-zlib --enable-bcmath --with-bz2 --enable-calendar --with-curl --enable-dba=shared --with-enchant --enable-exif --with-ffi --enable-ftp --enable-gd --with-external-gd --with-webp --with-jpeg --with-xpm --with-freetype --enable-gd-jis-conv --with-gettext --with-gmp --with-mhash --with-imap --with-kerberos --with-imap-ssl --enable-intl --with-ldap --with-ldap-sasl --enable-mbstring --with-mysqli --enable-pcntl --with-pdo-mysql --with-pspell --with-libedit --with-readline --enable-shmop --with-snmp --enable-soap --enable-sockets --enable-sysvmsg --enable-sysvsem --enable-sysvshm --with-tidy --with-expat --with-xmlrpc --with-xsl --enable-zend-test=shared --with-zip --enable-mysqlnd"
+	fi
+
 	pushd /tmp/php-$PHP_VER
-	test ! -f Makefile && EXTENSION_DIR=/opt/lamp/lib/extensions ./configure CFLAGS=-O2 CXXFLAGS=-O2 --prefix=/opt/lamp --with-config-file-path=/opt/lamp/etc --with-config-file-scan-dir=/opt/lamp/etc/php.d --with-apxs2=/opt/lamp/bin/apxs --enable-inline-optimization --enable-maintainer-zts --with-tsrm-pthreads --enable-phpdbg --with-openssl --with-kerberos --with-system-ciphers --with-zlib --enable-bcmath --with-bz2 --enable-calendar --with-curl --enable-dba=shared --with-enchant --enable-exif --with-ffi --enable-ftp --enable-gd --with-external-gd --with-webp --with-jpeg --with-xpm --with-freetype --enable-gd-jis-conv --with-gettext --with-gmp --with-mhash --with-imap --with-kerberos --with-imap-ssl --enable-intl --with-ldap --with-ldap-sasl --enable-mbstring --with-mysqli --enable-pcntl --with-pdo-mysql --with-pspell --with-libedit --with-readline --enable-shmop --with-snmp --enable-soap --enable-sockets --enable-sysvmsg --enable-sysvsem --enable-sysvshm --with-tidy --with-expat --with-xmlrpc --with-xsl --enable-zend-test=shared --with-zip --enable-mysqlnd
+	test ! -f Makefile && EXTENSION_DIR=/opt/$LAMP/lib/extensions ./configure CFLAGS=-O2 CXXFLAGS=-O2 --prefix=/opt/$LAMP --with-config-file-path=/opt/$LAMP/etc --with-config-file-scan-dir=/opt/$LAMP/etc/php.d --with-apxs2=/opt/$LAMP/bin/apxs $PHP_CFG
 	make -j4
 	make install
-	test ! -d /opt/lamp/etc && mkdir /opt/lamp/etc
-	\cp php.ini-* /opt/lamp/etc/
-	test ! -f /opt/lamp/etc/php.ini && \cp php.ini-development /opt/lamp/etc/php.ini
+	test ! -d /opt/$LAMP/etc && mkdir /opt/$LAMP/etc
+	\cp php.ini-* /opt/$LAMP/etc/
+	test ! -f /opt/$LAMP/etc/php.ini && \cp php.ini-development /opt/$LAMP/etc/php.ini
 	popd
 fi
 
@@ -86,13 +104,13 @@ fi
 # mariadb
 #############################################
 
-if test ! -f /opt/lamp/bin/mysqld; then
+if test ! -f /opt/$LAMP/bin/mysqld; then
 	test ! -d /tmp/mariadb-$MY_VER && tar -xvf mariadb-$MY_VER.tar.gz -C /tmp
 
 	pushd /tmp/mariadb-$MY_VER
 	test ! -d build && mkdir build
 	pushd build
-	test ! -f Makefile && cmake .. -DCMAKE_INSTALL_PREFIX=/opt/lamp -DMYSQL_DATADIR=/opt/lamp/var/mysql -DSYSCONFDIR=/opt/lamp/etc -DWITHOUT_TOKUDB=1 -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWITH_ARCHIVE_STPRAGE_ENGINE=1 -DWITH_BLACKHOLE_STORAGE_ENGINE=1 -DWIYH_READLINE=1 -DWIYH_SSL=system -DVITH_ZLIB=system -DWITH_LOBWRAP=0 -DMYSQL_UNIX_ADDR=/opt/lamp/var/mysql/mysql.sock -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci
+	test ! -f Makefile && cmake .. -DCMAKE_INSTALL_PREFIX=/opt/$LAMP -DMYSQL_DATADIR=/opt/$LAMP/var/mysql -DSYSCONFDIR=/opt/$LAMP/etc -DWITHOUT_TOKUDB=1 -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWITH_ARCHIVE_STPRAGE_ENGINE=1 -DWITH_BLACKHOLE_STORAGE_ENGINE=1 -DWIYH_READLINE=1 -DWIYH_SSL=system -DVITH_ZLIB=system -DWITH_LOBWRAP=0 -DMYSQL_UNIX_ADDR=/opt/$LAMP/var/mysql/mysql.sock -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci
 	make -j4
 	make install
 	popd
